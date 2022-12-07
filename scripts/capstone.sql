0ebff966657f54385507d2f03a01fdab64e201d8
CeciliaNd <Ceciliakwizer@gmail.com>
2022-12-06 20:06:18 -0600

    working on my question

--- scripts/capstone.sql
+++ scripts/capstone.sql
@@ -1,12 +1,11 @@
 /*Which state/region has the most 4 year college graduates from 2010-2014?*/
-
-
-
 -----------------------------------------------------------------------------
-select distinct state_name as state, census_region_name as region, persons_25_w_4_yrs_college
+select distinct state_name as state, sum(persons_25_w_4_yrs_college) AS persons_25_w_4_yrs_college, case
+                            when census_region_name IS NULL THEN 'N/A' ELSE census_region_name END AS census_region_name
 from educationdata
 where year = '2010-2014'
-group by state_name, census_region_name,persons_25_w_4_yrs_college 
-order by state_name 
+group by state_name, census_region_name, educationdata.persons_25_w_4_yrs_college
+--order by persons_25_w_4_yrs_college desc
+
 
 
