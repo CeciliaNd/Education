d16f1a4372735cdf861f4942a25e9a2b6ccd5216
CeciliaNd <Ceciliakwizer@gmail.com>
2022-12-08 19:09:15 -0600

    finishing up my questions

--- scripts/capstone.sql
+++ scripts/capstone.sql
@@ -1,23 +1,32 @@
 /*Which state/region has the most 4 year college graduates from 2010-2014?*/
 -----------------------------------------------------------------------------
 select distinct state_name as state, 
-sum(persons_25_w_4_yrs_college) AS persons_25_w_4_yrs_college,
-case
- when census_region_name IS NULL 
-    THEN 'N/A' ELSE census_region_name 
-        END AS census_region_name
+sum(persons_25_w_4_yrs_college) AS persons_25_w_4_yrs_college, census_region_name
 from educationdata
 where year = '2010-2014'
 group by state_name, census_region_name
 order by persons_25_w_4_yrs_college desc
 
-/*Which gender has the most graduates?*/
------------------------------------------
-select max(population_male_25_44) AS male ,max(population_female_25_44) AS female --sum(persons_25_w_4_yrs_college) as degrees
-from popdemodata 
-order by persons_25_w_4_yrs_college desc
---where year = '1975'
 
-select year 
-from popdemodata
+
+/*Which year had the most graduates?*/
+-----------------------------------------
+select year, sum(persons_25_w_4_yrs_college ) as graduates
+from popdemodata 
+group by year
+order by year desc
+
+
+/*which state has the most people with a hs diploma or more*/   
+---------------------------------------------------------------
+select sum(persons_25_w_hs_dipl_or_more) AS persons_25_w_hs_diplo_or_more, state_name
+from educationdata
+group by state_name
+order by state_name
+
+
+/*which state has the most people with a hs diploma +25 yrs old*/
+
+
+
 
