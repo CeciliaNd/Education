24e2137aa851f121009c5b39c2acb92ee4f7ca32
CeciliaNd <Ceciliakwizer@gmail.com>
2022-12-15 16:18:22 -0600

    adding a new query

--- scripts/capstone.sql
+++ scripts/capstone.sql
@@ -1,6 +1,11 @@
---does income play a role in graduates of 2010-2014?
-select GeoName
-from state_gdp
-where 1997
+--does race play a role in graduates of 2010-2014?
+select r.white ,r.black, r.hispanic, r.asian_pacific_islander, r.american_indian_alaska_native, e.state_name, sum(e.persons_25_w_4_yrs_college) as persons_25_w_4_yrs_college
+from raceandsex as r
+join educationdata as e
+on r.year = r.year
+where e.year = '2010-2014'
+group by r.white, e.state_name, r.black, r.hispanic, r.asian_pacific_islander, r.american_indian_alaska_native
+
+
 
 
